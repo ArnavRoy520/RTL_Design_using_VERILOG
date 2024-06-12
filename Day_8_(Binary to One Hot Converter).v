a0ee9e4d26d71511d0647771fde2a976b0936eb0
Arnav Roy <143030580+ArnavRoy520@users.noreply.github.com>
2024-06-12 22:40:56 +0530

    Update Day_8_(Binary to One Hot Converter).v

--- Day_8_(Binary to One Hot Converter).v
+++ Day_8_(Binary to One Hot Converter).v
@@ -1,13 +1,33 @@
-// Binary to one-hot
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: -
+// Engineer: Arnav Roy
+// 
+// Create Date: 12.06.2024 22:23:32
+// Design Name: Binary to one hot code
+// Module Name: one_hot
+// Project Name: Binary to one hot code
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
-module day8#(
-  parameter BIN_W       = 4,
-  parameter ONE_HOT_W   = 16
+
+module one_hot#(
+    parameter binary_word = 4,
+    parameter one_hot_word = 16
 )(
-  input   wire[BIN_W-1:0]     bin_i,
-  output  wire[ONE_HOT_W-1:0] one_hot_o
-);
-
-  assign one_hot_o = 1'b1<<bin_i;
-
+    input wire [binary_word-1:0] bin_i,
+    output wire [one_hot_word-1:0] one_hot_o
+    );
+    
+    assign one_hot_o = 1'b1<<bin_i;
+    
 endmodule
